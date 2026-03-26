4f07fd240393fe2c7b4011048a78980ba2f1ff6e
koljaPl <nikolya.plugin@gmail.com>
2026-03-26 12:39:23 +0100

    Added code standarts (code standarts and test-case sitations) for olimpiads and fast coding

---
+++ solving_process/test_cases.py
@@ -0,0 +1,16 @@
+import sys
+# sys.setrecursionlimit(200000)
+
+input = sys.stdin.readline
+
+def main():
+    t = int(input())
+
+    for _ in range(t):
+        # data = list(map(int, input().split()))
+
+        continue
+
+        # sys.stdout.write(str(result) + '\n')
+
+main()
